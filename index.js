5e2493d5f6fb15ec2fd4ff201fca2752b6dcd8b1
jaimegalvis <jaime.galvis.r@gmail.com>
2021-03-27 12:00:43 -0500

    mostrar hola mundo

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>{
     console.log('ruta: ',req.url);
-    // switch(req.url){
-    //     case '/':
+    switch(req.url){
+        case '/':
+            res.writeHead(200,{'Content-type':'application/json'})
+            // res.statusCode(200);
+            // res.statusMessage('Todo bien');
+            res.end(JSON.stringify('hola mundo'));
+            break;
 
-    //     case'/':
+        // case'/':
 
+        default:
+            res.writeHead(200,{'Content-type':'text/plain'})
+            // res.statusCode(404);
+            res.end('No encontrado')
 
-    // }
+    }
 
 
 
